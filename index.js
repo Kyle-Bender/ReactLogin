e58b94e8786872171b69d8212672840b6ea9a074
Kyle Bender <kyle.bender128@gmail.com>
2023-01-03 20:06:56 -0500

    copy html & css

--- index.js
+++ index.js
@@ -1,4 +1,52 @@
 ReactDOM.render(
-    <ul><li>Thing 1</li><li>Thing 2</li><li>Thing 3</li></ul>,
+    <html>
+    <head>
+
+        <link rel="stylesheet" href="index.css">
+
+    </head>
+
+    <body>
+
+            <div class="login-div">
+                <div class="logo">
+                    <img src="logo.jpg" alt="" width="120">
+                </div>
+
+                <div class="title">Black Matrix</div>
+                <div class="sub-title">Technology & Design</div>
+
+                <div class="form">
+
+                    <div class="username">
+                        <input type="text" placeholder="Username or Email">
+                    </div>
+
+                    <div class="password">
+                        <input type="password" placeholder="Password">
+                    </div>
+
+                    <div class="options">
+                        <div class="remember-me"></div>
+                        <input id="remember-me" type="checkbox">
+                        <label for="remember-me">Remember Me?</label>
+                    </div>
+                    
+                    <div class="forgot-password">
+                        <a href="#">Forgot Password?</a>
+                    </div>
+
+                    <button class="signin-btn">LOGIN</button>
+                    <div class="sign-up">
+                        <a href="#"> New to Black Matrix? Sign up here.</a>
+                    </div>
+
+                </div>
+
+            </div>
+
+
+    </body>
+</html>,
     document.getElementById("root")
 )
